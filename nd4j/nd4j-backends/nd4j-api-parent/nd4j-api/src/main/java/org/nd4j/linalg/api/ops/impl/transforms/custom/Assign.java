ea3f30e48ec395d472627cfbf224e22b05d9b6f2
Robert Altena <Rob@Ra-ai.com>
2019-12-23 08:33:45 +0900

    fix ScalarEquals, Assign.
    Signed-off-by: Robert Altena <Rob@Ra-ai.com>

--- nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/api/ops/impl/transforms/custom/Assign.java
+++ nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/api/ops/impl/transforms/custom/Assign.java
@@ -24,6 +24,7 @@ import org.nd4j.linalg.api.buffer.DataType;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.api.ops.DynamicCustomOp;
 import org.nd4j.linalg.api.ops.Op;
+import org.nd4j.linalg.factory.Nd4j;
 import org.tensorflow.framework.AttrValue;
 import org.tensorflow.framework.GraphDef;
 import org.tensorflow.framework.NodeDef;
@@ -47,7 +48,7 @@ public class Assign extends DynamicCustomOp {
     }
 
     public Assign(INDArray x, INDArray y ) {
-        this( new INDArray[] {x}, new INDArray[] {y}); // TODo check. modelled after the SameDiff constructor,
+        this( new INDArray[]{y ,x},new INDArray[]{y}); // TODO: Still check. y cannot be null, must be same shape as x.
     }
 
     @Override
